918ec9b7eab746a0da4b6c7f5a643d5349e879cd
Linnea <bark_linnea@hotmail.com>
2019-03-07 11:48:03 +0100

    Added picture to background

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from guizero import App, Text
+from guizero import App, Text, TextBox, PushButton, Slider, Picture
 
-app = App(title="Den Sociala Roboten")
+app = App(title="Den sociala roboten", height=480, width=800)
+
+eyes = Picture(app, image="eyes1.gif")
 
 app.display()
