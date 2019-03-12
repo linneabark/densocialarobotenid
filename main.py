f416a387eafc199d6a1dedf2f73aa4b34373f3ce
Linnea <bark_linnea@hotmail.com>
2019-03-12 09:51:57 +0100

    removed roboten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from guizero import App, Text, TextBox, PushButton, Slider, Picture
 
-app = App(title="Den sociala roboten", height=480, width=800)
+app = App(title="Den sociala", height=480, width=800)
 
 eyes = Picture(app, image="eyes1.gif")
 
