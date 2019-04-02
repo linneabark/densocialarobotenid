63f65633a545dd74476bbb3191c2706781a1a650
pergel4 <pernilla@gellerman.nu>
2019-04-02 09:41:40 +0200

    h

--- schedule_app.py
+++ schedule_app.py
@@ -4,10 +4,16 @@
 from kivy.app import App
 
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition, SwapTransition, SlideTransition
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.pagelayout import PageLayout
+
+
 
 class ScheduleApp(App):
     def build(self):
-        return Label(text='hej')
+        return PageLayout()
 
 if __name__ == '__main__':
     ScheduleApp().run()
