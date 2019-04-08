7e6a3f383ba025760e37ca7d937523c90c52c271
Linnea <bark_linnea@hotmail.com>
2019-04-08 11:15:03 +0200

    Dont remember honestly

--- main.py
+++ main.py
@@ -77,7 +77,6 @@ class Manager(ScreenManager):
         self.add_widget(Appview(name='appview'))
         self.add_widget(MathScreen(name='math'))
         self.add_widget(RPSScreen(name='rps'))
-        self.add_widget(ScheduleSScreen(name='schedule2'))
 
 
 
